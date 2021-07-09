68e30e4e199d96efbb1afd937c649b3b05a22ab4
Vrithi-hub <87144508+Vrithi-hub@users.noreply.github.com>
2021-07-09 20:48:25 +0530

    Update main_module.py

--- main_module.py
+++ main_module.py
@@ -14,10 +14,8 @@ with open('log.txt', "rb") as f:
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
-# create formatter and add it to the handler
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
-# add the handler to the logger
 logger.addHandler(handler)
 
 
