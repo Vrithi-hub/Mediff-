dc9ec8af2c38b9270e8d740e0467320e7d1ad122
Vrithi-hub <87144508+Vrithi-hub@users.noreply.github.com>
2021-07-10 16:25:40 +0530

    Delete Parse_log.py

--- Parse_log.py
+++
@@ -1,7 +0,0 @@
-import logging
-logging.basicConfig(filename='log.txt',filemode='a',level=logging.DEBUG)
-logging.debug('this is a debug message')
-logging.warning('this s warning')
-logger = logging.getLogger(__name__)
-
-
